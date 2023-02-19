52c2eb411233d80b2a26b792cefcae92de359871
Tim Hunter <thunte27@uwo.ca>
2023-02-19 11:39:09 -0500

    add utility functions for oauth

---
+++ app/utils.py
@@ -0,0 +1,22 @@
+from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from controller import get_user_by_email
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def authenticate_user(db, username: str, password: str):
+    user = get_user_by_email(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
