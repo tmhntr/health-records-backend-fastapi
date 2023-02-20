dfdf78aa8fc21c8639d513e82217a91ae5bd7fc7
Tim Hunter <thunte27@uwo.ca>
2023-02-19 19:28:12 -0500

    remove commented out code

--- app/main.py
+++ app/main.py
@@ -11,19 +11,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:8080",
-#     "http://localhost:8081",
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 app.include_router(routes.router)
 
