8e35bc2054b197e3d34b782c0c8683545a76cf97
Tim Hunter <thunte27@uwo.ca>
2023-02-20 22:44:24 -0500

    remove .vscode settings

--- app/auth.py
+++ app/auth.py
@@ -29,3 +29,21 @@ def create_access_token(data: dict, expires_delta=timedelta(minutes=30)) -> str:
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+# OAuth settings
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_SECRET') or None
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH_CLIENT_ID') or None
+if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
+    raise BaseException('Missing env variables')
+
+# Set up oauth
+config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID,
+               'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+starlette_config = Config(environ=config_data)
+oauth = OAuth(starlette_config)
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'},
+)
