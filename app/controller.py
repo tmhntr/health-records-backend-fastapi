1c314942b695513dc7479a7632582ce715a3fdb1
Tim Hunter <thunte27@uwo.ca>
2023-02-23 04:08:48 -0500

    add record functions to controller module

--- app/controller.py
+++ app/controller.py
@@ -31,15 +31,53 @@ def create_user(db: Session, user: schemas.UserCreate) -> models.User:
     return db_user
 
 
-def get_records(db: Session, user_id: int, skip: int = 0, limit: int = 100) -> list[models.HealthRecord]:
-    result = db.scalars(select(models.HealthRecord).where(
-        models.HealthRecord.owner_id == user_id).order_by(models.HealthRecord.id).offset(skip).limit(limit))
+def get_records(db: Session, user_id: int, skip: int = 0, limit: int = 100, sort_by: str = None, sort_dir: str = None, filter: str = None) -> list[models.HealthRecord]:
+    
+    result = select(models.HealthRecord).where(
+        models.HealthRecord.owner_id == user_id)
+        
+    if filter:
+        for key, value in filter.items():
+            result = result.where(getattr(models.HealthRecord, key) == value)
+
+    if sort_by:
+        if sort_dir == "desc":
+            result = result.order_by(getattr(models.HealthRecord, sort_by).desc())
+        else:
+            result = result.order_by(getattr(models.HealthRecord, sort_by))
+    else:
+        result = result.order_by(models.HealthRecord.id)
+
+    result = result.offset(skip).limit(limit)
+
+    result = db.scalars(result)
     return result.all()
 
+def get_record(db: Session, record_id: int) -> models.HealthRecord:
+    result = db.scalars(select(models.HealthRecord).where(models.HealthRecord.id == record_id))
+    return result.first()
 
 def create_user_record(db: Session, record: schemas.RecordCreate, user_id: int) -> models.HealthRecord:
     db_record = models.HealthRecord(**record.dict(), owner_id=user_id)
     db.add(db_record)
     db.commit()
     db.refresh(db_record)
+    return db_record
+
+def update_record(db: Session, record: schemas.RecordUpdate, record_id: int) -> models.HealthRecord:
+    db_record = get_record(db, record_id=record_id)
+    if db_record is None:
+        return None
+    for key, value in record.dict(exclude_unset=True).items():
+        setattr(db_record, key, value)
+    db.commit()
+    db.refresh(db_record)
+    return db_record
+
+def delete_record(db: Session, record_id: int) -> models.HealthRecord:
+    db_record = get_record(db, record_id=record_id)
+    if db_record is None:
+        return None
+    db.delete(db_record)
+    db.commit()
     return db_record
