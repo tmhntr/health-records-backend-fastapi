053e78f6565a3e6dd3ed17c59dbbaa860fd91e2b
Tim Hunter <thunte27@uwo.ca>
2023-02-23 04:09:54 -0500

    refactor db to use local env module

--- app/database.py
+++ app/database.py
@@ -1,12 +1,10 @@
-from os import environ
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from app.env import env
 
-load_dotenv(".env.local")
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = environ['DATABASE_LOGIN_URL']
+SQLALCHEMY_DATABASE_URL = env.get('DATABASE_LOGIN_URL')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
