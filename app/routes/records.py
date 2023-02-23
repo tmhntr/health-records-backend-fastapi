7d8d99c2420c223cfe0b69e2738c8b3c9dfff465
Tim Hunter <thunte27@uwo.ca>
2023-02-23 04:10:44 -0500

    add sorting and filtering to get records

--- app/routes/records.py
+++ app/routes/records.py
@@ -11,13 +11,16 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+
 @router.post("/", response_model=schemas.Record)
 def create_record(record: schemas.RecordCreate, user: models.User = Depends(get_current_user), db: Session = Depends(get_db)):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    db_record = controller.create_user_record(db=db, record=record, user_id=user.id)
+    db_record = controller.create_user_record(
+        db=db, record=record, user_id=user.id)
     return schemas.Record.from_orm(db_record)
+
 
 @router.delete("/{record_id}", response_model=schemas.Record)
 def delete_record(record_id: int, user: models.User = Depends(get_current_user), db: Session = Depends(get_db)):
@@ -33,12 +36,14 @@ def delete_record(record_id: int, user: models.User = Depends(get_current_user),
     db.commit()
     return schemas.Record.from_orm(db_record)
 
+
 @router.get("/", response_model=list[schemas.Record])
-def read_records(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
+def read_records(skip: int = 0, limit: int = 100, sort_by: str = None, sort_dir: str = None, filter: str = None, db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    records = controller.get_records(db, skip=skip, limit=limit, user_id=user.id)
+    records = controller.get_records(
+        db, skip=skip, limit=limit, user_id=user.id)
     return [schemas.Record.from_orm(record) for record in records]
 
 
@@ -53,6 +58,3 @@ def read_record(record_id: int, db: Session = Depends(get_db), user: models.User
     if db_record.owner_id != user.id:
         raise HTTPException(status_code=403, detail="Forbidden")
     return schemas.Record.from_orm(db_record)
-    
-
-
