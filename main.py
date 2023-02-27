b55693bb8a774bdc8d7de1fc7811ff98f260e927
Tim Hunter <thunte27@uwo.ca>
2023-02-27 00:08:58 -0500

    refactor: change main.py to always run app

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from uvicorn import run
-from src import app
+from src.app import app
 
 from os import environ
 
 PORT = int(environ.get("PORT", 8000))
 
-if __name__ == "__main__":
-    run(app, host="0.0.0.0", port=PORT)
+run(app, host="0.0.0.0", port=PORT)
